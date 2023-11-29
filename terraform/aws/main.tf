eda028d1de37d1bf95e9698c65f78440e466a164
LucasMeres97 <rm551185@fiap.com.br>
2023-11-29 19:38:08 -0300

    teste 2

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,4 +1,6 @@
-// create two ec2 with load balancer attached to them. The ec2s must have a apache server html page
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_vpc" "web" {
   cidr_block = "172.0.0.0/16"
@@ -10,6 +12,7 @@ resource "aws_subnet" "web-1" {
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
+
 resource "aws_subnet" "web-2" {
   vpc_id                  = aws_vpc.web.id
   cidr_block              = "172.0.3.0/24"
@@ -17,13 +20,13 @@ resource "aws_subnet" "web-2" {
   map_public_ip_on_launch = true
 }
 
-
 resource "aws_internet_gateway" "web" {
   vpc_id = aws_vpc.web.id
 }
 
 resource "aws_route_table" "web" {
   vpc_id = aws_vpc.web.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.web.id
@@ -34,6 +37,7 @@ resource "aws_route_table_association" "web-1" {
   subnet_id      = aws_subnet.web-1.id
   route_table_id = aws_route_table.web.id
 }
+
 resource "aws_route_table_association" "web-2" {
   subnet_id      = aws_subnet.web-2.id
   route_table_id = aws_route_table.web.id
@@ -41,13 +45,21 @@ resource "aws_route_table_association" "web-2" {
 
 resource "aws_security_group" "web" {
   name        = "web"
-  description = "Allow web inbound traffic"
+  description = "Permite tráfego web de entrada"
   vpc_id      = aws_vpc.web.id
 
   ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -78,11 +90,30 @@ resource "aws_instance" "web-2" {
   subnet_id                   = aws_subnet.web-1.id
   vpc_security_group_ids      = [aws_security_group.web.id]
   user_data                   = base64encode(data.template_file.user_data.rendered)
-
 }
+
+resource "aws_instance" "web-3" {
+  ami                         = "ami-0230bd60aa48260c6"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1b"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.web-2.id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  user_data                   = base64encode(data.template_file.user_data.rendered)
+}
+
+resource "aws_instance" "web-4" {
+  ami                         = "ami-0230bd60aa48260c6"
+  instance_type               = "t2.micro"
+  availability_zone           = "us-east-1b"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.web-2.id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  user_data                   = base64encode(data.template_file.user_data.rendered)
+}
+
 data "template_file" "user_data" {
   template = file("./script/user_data.sh")
-
 }
 
 resource "aws_lb" "lb" {
@@ -93,15 +124,15 @@ resource "aws_lb" "lb" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "tg-nadin"
+  name     = "tg-meres"
   protocol = "HTTP"
-  port     = "80"
+  port     = 80
   vpc_id   = aws_vpc.web.id
 }
 
 resource "aws_lb_listener" "ec2_lb_listener" {
   protocol          = "HTTP"
-  port              = "80"
+  port              = 80
   load_balancer_arn = aws_lb.lb.arn
 
   default_action {
@@ -109,6 +140,7 @@ resource "aws_lb_listener" "ec2_lb_listener" {
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
+
 resource "aws_lb_target_group_attachment" "web-1" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.web-1.id
@@ -118,5 +150,17 @@ resource "aws_lb_target_group_attachment" "web-1" {
 resource "aws_lb_target_group_attachment" "web-2" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.web-2.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web-3" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web-3.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web-4" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web-4.id
   port             = 80
 }
