0a1eae67fc53cdbdf9f1359693eb403bcd09be40
LucasMeres97 <rm551185@fiap.com.br>
2023-11-29 19:51:18 -0300

    teste 6

--- terraform/aws/provider.tf
+++ terraform/aws/provider.tf
@@ -1,33 +1,24 @@
 # PROVIDER
 terraform {
-
   required_version = "~> 1.5"
-
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.34"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2"
-    }
+    aws = "~> 4.34"
+    template = "~> 2.2"
   }
-
-  # backend "s3" {
-  #   bucket         = "tf-state-efs-bucket"
-  #   key            = "terraform.tfstate"
-  #   dynamodb_table = "tf-state-efs-table"
-    
-  #   region         = "us-east-1"
-  # }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "web" {
   cidr_block = "172.0.0.0/16"
+}
+
+resource "aws_subnet" "web-1" {
+  vpc_id                  = aws_vpc.web.id
+  cidr_block              = "172.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
