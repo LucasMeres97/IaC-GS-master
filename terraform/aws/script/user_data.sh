e157a6313ac15a764ad9e4b4dca7027d8fd23c97
LucasMeres97 <rm551185@fiap.com.br>
2023-11-29 21:23:16 -0300

    teste 17

--- terraform/aws/script/user_data.sh
+++ terraform/aws/script/user_data.sh
@@ -11,7 +11,7 @@ sudo systemctl enable httpd
     
 echo "Install application"
 cd /tmp
-git clone https://github.com/NaderSouza/IaC-GS
+git clone https://github.com/LucasMeres97/IaC-GS-master.git
 mkdir /var/www/html
 cp /tmp/IaC-GS/app/*.html /var/www/html
 
